da2ef6274c7528a73a51687158c375517145a739
Sebastien Waegeneire <sebastien@waegeneire.com>
2026-01-31 22:05:32 +0100

    refactor: throw explicit error when root element not found

--- app/main.tsx
+++ app/main.tsx
@@ -4,10 +4,9 @@ import "./index.css";
 import App from "./App.tsx";
 
 const root = document.getElementById("root");
-if (root) {
-  createRoot(root).render(
-    <StrictMode>
-      <App />
-    </StrictMode>,
-  );
-}
+if (!root) throw new Error("Root element not found");
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+);
